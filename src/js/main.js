9b5c25e1fe3178329420dc07d5baf1ec3053af77
John Doe <y.sementsiv@gmail.com>
2021-09-04 00:12:07 +0300

    fixed header

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,17 @@ $(document).ready(function() {
         $('.header__burger,.header__nav,.header__burger-line').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    $('.header__nav-link, .main, .footer').click(function(event) {
+    $('.header__nav-link, .hero, .why, .how, .booking-top, .booking-bottom, .testimonial, .footer').click(function(event) {
         $('.header__burger,.header__nav,.header__burger-line').removeClass('active');
         $('body').removeClass('lock');
     });
+});
+$(window).scroll(function() {
+    var position = $(window).scrollTop(),
+        block_position = $('.header').offset().top;
+    if (position > block_position) {
+        $('.header__nav').addClass('reverse');
+    } else {
+        $('.header__nav').removeClass('reverse');
+    }
 });
